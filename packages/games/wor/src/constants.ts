fa765f2cde2b19599b4ea9e901d8b0ef38f53ae1
Lutz Schwemer Panchez <shark@shark5060.net>
2026-08-31 19:39:46 +0200

    chore: remove comment

--- packages/games/wor/src/constants.ts
+++ packages/games/wor/src/constants.ts
@@ -68,7 +68,6 @@ export function rarityToStarRating(rarity: string): number {
 
 export const HERO_AWAKENING_MAX = 5;
 export const ARTIFACT_PROMOTION_MAX = 5;
-/** Owned demons start at level 1 in-game; 0 is only the unowned stored value. */
 export const DEMON_LEVEL_MIN = 1;
 
 export const ARTIFACT_GAUGE_FILLED = '▰';
